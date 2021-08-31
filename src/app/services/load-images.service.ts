fea1ce54cf9ae8c40e5c2034064f1d18960f2a17
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-08-31 18:38:43 -0500

    Upload images to Firebase - Storage

--- src/app/services/load-images.service.ts
+++ src/app/services/load-images.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+
+import { map } from 'rxjs/operators';
+
 import { FileItem } from '../models/file-item';
+
+import firebase from 'firebase';
+import 'firebase/storage';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/firestore';
+
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root',
@@ -11,7 +22,34 @@ export class LoadImagesService {
   constructor(public firestore: AngularFirestore) {}
 
   loadImagesFirebase(images: FileItem[]) {
-    console.log(images);
+    const storageRef = firebase.storage().ref();
+
+    for (const item of images) {
+      item.isGoing = true;
+      if (item.progress >= 100) {
+        continue;
+      }
+
+      const uploadTask: firebase.storage.UploadTask = storageRef
+        .child(`${this.FOLDER_IMAGES}/${item.nameFile}`)
+        .put(item.file);
+
+      uploadTask.on(
+        firebase.storage.TaskEvent.STATE_CHANGED,
+        (snapshot: firebase.storage.UploadTaskSnapshot) => {
+          (item.progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100),
+            () => {
+              console.log('Img ready');
+              item.isGoing = false;
+              this.saveImage({
+                name: item.nameFile,
+                url: item.url,
+              });
+            };
+        }
+      );
+    }
   }
 
   private saveImage(image: { name: string; url: string }) {
