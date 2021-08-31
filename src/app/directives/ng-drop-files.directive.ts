2d827fe75d72bb9b833a7740845d9d1a29b302ed
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-08-31 17:25:10 -0500

    File validations from the directive

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -6,11 +6,13 @@ import {
   Input,
   Output,
 } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 @Directive({
   selector: '[appNgDropFiles]',
 })
 export class NgDropFilesDirective {
+  @Input() files: FileItem[] = [];
   @Output() mouseOn: EventEmitter<boolean> = new EventEmitter();
 
   constructor() {}
@@ -23,5 +25,36 @@ export class NgDropFilesDirective {
   @HostListener('dragleave', ['$event'])
   public onDragLeave(event: any) {
     this.mouseOn.emit(false);
+  }
+
+  // VALIDATIONS
+  private _fileCanBeUploaded(file: File): boolean {
+    if (!this._fileWasAlreadyDroppeado(file.name) && this._isImage(file.type)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private _preventStop(event: any) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private _fileWasAlreadyDroppeado(nameFile: string): boolean {
+    for (const file of this.files) {
+      if (file.nameFile === nameFile) {
+        console.log('pepe');
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private _isImage(typeFile: string): boolean {
+    return typeFile === '' || typeFile === undefined
+      ? false
+      : typeFile.startsWith('image');
   }
 }
